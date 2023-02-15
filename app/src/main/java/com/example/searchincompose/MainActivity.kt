375a1b823b41e3f69af33264561427aee85f4308
mktowett <marvintowett@gmail.com>
2023-02-15 11:24:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/searchincompose/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.searchincompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.searchincompose.ui.theme.SearchInComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SearchInComposeTheme {
+                val viewModel = viewModel<MainActivityViewModel>()
+                val searchText by viewModel.searchText.collectAsState()
+                val persons by viewModel.person.collectAsState()
+                val isSearching by viewModel.isSearching.collectAsState()
+                
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                ) {
+                    TextField(
+                        value = searchText, 
+                        onValueChange = viewModel::onSearchTextChange,
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = {
+                            Text(text = "Search")
+                        }
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    if (isSearching){
+                        Box(modifier = Modifier.fillMaxSize()){
+                            CircularProgressIndicator(
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                    }
+
+                    LazyColumn(modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                    ){
+                        items(persons){ person ->
+                            Text(
+                                text = "${person.firstName} ${person.lastName}",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 16.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/searchincompose/MainActivityViewModel.kt
@@ -0,0 +1,84 @@
+package com.example.searchincompose
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+
+class MainActivityViewModel : ViewModel() {
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching = _isSearching.asStateFlow()
+
+    private val _persons = MutableStateFlow(allPersons)
+
+    //search logic
+    val person = searchText
+        .debounce(1000L)
+        .onEach {
+            _isSearching.update { true }
+        }.combine(_persons) { text, persons ->
+            if (text.isBlank()) {
+                persons
+            } else {
+                delay(2000L)
+                persons.filter {
+                    it.doesMatchSearchQuery(text)
+                }
+            }
+        }.onEach {
+            _isSearching.update { false }
+        }.stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            _persons.value
+        )
+
+
+    fun onSearchTextChange(text: String) {
+        _searchText.value = text
+    }
+
+}
+
+data class Person(
+    val firstName: String,
+    val lastName: String
+) {
+    fun doesMatchSearchQuery(query: String): Boolean {
+        val matchingCombinations = listOf(
+            "$firstName$lastName",
+            "$firstName $lastName",
+            "${firstName.first()} ${lastName.first()}"
+        )
+
+        return matchingCombinations.any {
+            it.contains(query, ignoreCase = true)
+        }
+    }
+}
+
+private val allPersons = listOf(
+    Person(
+        firstName = "Marvin",
+        lastName = "Towett"
+    ),
+    Person(
+        firstName = "Mercy",
+        lastName = "Joy"
+    ),
+    Person(
+        firstName = "Ian",
+        lastName = "Gitonga"
+    ),
+    Person(
+        firstName = "Davis",
+        lastName = "Rono"
+    ),
+    Person(
+        firstName = "Alex",
+        lastName = "Mwangi"
+    ),
+)
